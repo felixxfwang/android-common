32ecbb5dc150b7a77995883a4abd94c279b1087d
felixxfwang <felixxfwang@tencent.com>
2021-02-28 18:58:20 +0800

    add action button

--- navigation-ktx/src/main/java/androidx/navigation/ui/TiramisuOnDestinationChangedListener.java
+++ navigation-ktx/src/main/java/androidx/navigation/ui/TiramisuOnDestinationChangedListener.java
@@ -1,10 +1,13 @@
 package androidx.navigation.ui;
 
 import android.annotation.SuppressLint;
+import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 
 import org.tiramisu.navigation.OnDestinationChangedListener;
 
@@ -28,6 +31,14 @@ public class TiramisuOnDestinationChangedListener extends ActionBarOnDestination
             super.setTitle(title);
         } else {
             listener.setActionBarTitle(title);
+        }
+    }
+
+    @Override
+    public void onDestinationChanged(@NonNull NavController controller, @NonNull NavDestination destination, @Nullable Bundle arguments) {
+        super.onDestinationChanged(controller, destination, arguments);
+        if (listener != null) {
+            listener.onDestinationChanged(controller, destination, arguments);
         }
     }
 }
--- navigation-ktx/src/main/java/org/tiramisu/navigation/OnDestinationChangedListener.kt
+++ navigation-ktx/src/main/java/org/tiramisu/navigation/OnDestinationChangedListener.kt
@@ -1,5 +1,10 @@
 package org.tiramisu.navigation
 
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+
 interface OnDestinationChangedListener {
     fun setActionBarTitle(title: CharSequence)
+    fun onDestinationChanged(controller: NavController, destination: NavDestination, arguments: Bundle?) {}
 }
