f2698750af1e2292c6f1b45afe8ec6b351ec9781
felixxfwang <felixxfwang@tencent.com>
2021-03-04 14:28:56 +0800

    mappding updalod

--- crashlytics/src/main/java/org/tiramisu/crashlytics/ICrashlytics.kt
+++ crashlytics/src/main/java/org/tiramisu/crashlytics/ICrashlytics.kt
@@ -8,4 +8,12 @@ interface ICrashlytics {
 
     // 缓存64kb
     fun log(tag: String, message: String)
+
+
+    fun setCustomKey(key: String, value: Boolean)
+    fun setCustomKey(key: String, value: Int)
+    fun setCustomKey(key: String, value: String)
+    fun setCustomKey(key: String, value: Long)
+    fun setCustomKey(key: String, value: Float)
+    fun setCustomKey(key: String, value: Double)
 }
--- crashlytics/src/main/java/org/tiramisu/crashlytics/TFirebaseCrashlytics.kt
+++ crashlytics/src/main/java/org/tiramisu/crashlytics/TFirebaseCrashlytics.kt
@@ -7,10 +7,36 @@ import com.google.firebase.crashlytics.FirebaseCrashlytics
  * @author felixxfwang
  */
 class TFirebaseCrashlytics: ICrashlytics {
+    
     override fun initialize(application: Application) {
+        setCustomKey("TFirebaseCrashlytics", true)
     }
 
     override fun log(tag: String, message: String) {
         FirebaseCrashlytics.getInstance().log("$tag:$message")
+    }
+
+    override fun setCustomKey(key: String, value: Boolean) {
+        FirebaseCrashlytics.getInstance().setCustomKey(key, value)
+    }
+
+    override fun setCustomKey(key: String, value: Int) {
+        FirebaseCrashlytics.getInstance().setCustomKey(key, value)
+    }
+
+    override fun setCustomKey(key: String, value: String) {
+        FirebaseCrashlytics.getInstance().setCustomKey(key, value)
+    }
+
+    override fun setCustomKey(key: String, value: Long) {
+        FirebaseCrashlytics.getInstance().setCustomKey(key, value)
+    }
+
+    override fun setCustomKey(key: String, value: Float) {
+        FirebaseCrashlytics.getInstance().setCustomKey(key, value)
+    }
+
+    override fun setCustomKey(key: String, value: Double) {
+        FirebaseCrashlytics.getInstance().setCustomKey(key, value)
     }
 }
