08bf6f992e2277ded20f6810baeb1318c637d35d
felixxfwang <felixxfwang@tencent.com>
2021-03-09 12:58:33 +0800

    fix item decoration

---
+++ widgets/src/main/java/org/tiramisu/widgets/DividerDecoration.kt
@@ -0,0 +1,10 @@
+package org.tiramisu.widgets
+
+import android.content.Context
+import androidx.recyclerview.widget.DividerItemDecoration
+
+/**
+ * @author felixxfwang
+ */
+class DividerDecoration(context: Context, orientation: Int) : DividerItemDecoration(context, orientation) {
+}
