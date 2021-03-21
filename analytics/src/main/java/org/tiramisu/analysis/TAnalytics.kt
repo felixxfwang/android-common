091adaee42d6b6787e979c5f0bb9de756dbb0fd6
felixxfwang <felixxfwang@tencent.com>
2021-03-21 12:04:56 +0800

    opt modular depends

--- analytics/src/main/java/org/tiramisu/analysis/TAnalytics.kt
+++ analytics/src/main/java/org/tiramisu/analysis/TAnalytics.kt
@@ -2,6 +2,7 @@ package org.tiramisu.analysis
 
 import android.app.Application
 import com.google.firebase.analytics.FirebaseAnalytics
+import java.util.concurrent.atomic.AtomicBoolean
 
 /**
  * @author felixxfwang
@@ -12,8 +13,15 @@ object TAnalytics : IAnalytics by analytics {
 
     const val PARAM_METHOD = FirebaseAnalytics.Param.METHOD
 
+    private val isInitialized = AtomicBoolean(false)
+
+    fun isInitialized() = isInitialized.get()
+
+    @Synchronized
     override fun initialize(application: Application, userProperties: Map<String, String>?) {
-        analytics.initialize(application, userProperties)
-        application.registerActivityLifecycleCallbacks(TAnalyticsLifecycleCallbacks())
+        if (isInitialized.compareAndSet(false, true)) {
+            analytics.initialize(application, userProperties)
+            application.registerActivityLifecycleCallbacks(TAnalyticsLifecycleCallbacks())
+        }
     }
 }
